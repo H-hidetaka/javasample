761aad58c8ba6c3b42f17b4d01d43028cce6497f
H-hidetaka <sansanhidetaka@gmail.com>
2022-03-29 23:28:40 +0900

    [Add]javasample

---
+++ sample/src/sample/Main.java
@@ -0,0 +1,36 @@
+package sample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("相澤");
+		System.out.println("相澤");
+		System.out.println("アイウエオ");
+		System.out.println("５３さい");
+		System.out.println("お酒が好きです。");
+		System.out.println("相澤");
+		System.out.println("相澤");
+		System.out.println("相澤");		
+		System.out.println("31 + 31の計算をします");		
+		System.out.println(31 + 31);		
+		System.out.println(31 - 31);		
+		System.out.println(-5 * 2);		
+		System.out.println(6 * 6 * 3.14);		
+//		System.out.println("答えは" - 31);		
+		System.out.println("答えは" + 64);		
+		System.out.println(31 - 31);	
+		int x;
+		x = 6;
+		System.out.println(x * x * 3.14);
+	}
+}
+
+public class MyDairy {
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println(x * x * 3.14);
+		System.out.println();
+		
+	}
+}
